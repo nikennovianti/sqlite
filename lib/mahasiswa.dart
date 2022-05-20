3c7c9a648c1d083840cda6e2fdba869f5db06f41
nikennovianti <75300844+nikennovianti@users.noreply.github.com>
2022-05-20 18:47:57 +0800

    Update mahasiswa.dart

--- lib/mahasiswa.dart
+++ lib/mahasiswa.dart
@@ -4,28 +4,11 @@ class Mahasiswa {
   late String nama;
   late String nim;
 
-  // Mahasiswa(
-
-  //   this._nama,
-  //   this._nim
-  // );
-
   Mahasiswa(
     this.id,
     this.nama,
     this.nim
   );
-
-  // int? get id => _id;
-  // String? get nama => _nama;
-  // int? get nim => _nim;
-
-  // set nama(String? value) {
-  //   _nama = value;
-  // }
-  //  set nim(int? value) {
-  //   _nim = value;
-  // }
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
